d9932b20240376fbc2c6a8a75639e33c9b3b0e84
Lina <Benhaj.L@outlook.com>
2025-05-20 16:15:13 +0200

    Eerste commit: Spring Boot project met MySQL setup

---
+++ src/main/java/be/ehb/bakkerij/BakkerijApplication.java
@@ -0,0 +1,13 @@
+package be.ehb.bakkerij;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakkerijApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BakkerijApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/ehb/bakkerij/BakkerijApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ehb.bakkerij;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakkerijApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
